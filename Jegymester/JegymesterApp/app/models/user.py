d525efaee04e2a94384fea0f92bdf96c77d801a0
akuas <sztupyakos@gmail.com>
2026-03-04 11:12:34 +0100

    finished user model

--- Jegymester/JegymesterApp/app/models/user.py
+++ Jegymester/JegymesterApp/app/models/user.py
@@ -6,15 +6,6 @@ from typing import List, Optional
 from werkzeug.security import generate_password_hash, check_password_hash
 from marshmallow import Schema, fields
 
-
-UserRole = Table(
-    "userroles",
-    Base.metadata,
-    Column("user_id", ForeignKey("users.id")),
-    Column("role_id", ForeignKey("roles.id"))
-)
-
-
 class User(db.Model):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -23,10 +14,9 @@ class User(db.Model):
     password: Mapped[str] = mapped_column(String(30))
     phone : Mapped[str] = mapped_column(String(30))
     
-    roles: Mapped[List["Role"]] = relationship(secondary=UserRole, back_populates="users")
-   
-    address_id: Mapped[int] = mapped_column(ForeignKey("addresses.id"))
-    address : Mapped["Address"] = relationship(back_populates="user", lazy=True)
+    role: Mapped[str] = mapped_column(String(30))
+
+    tickets: Mapped[List["Ticket"]] = relationship(back_populates="user", lazy=True)
         
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, name={self.name!s}, email={self.email!r})"

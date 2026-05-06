afc2565849224ff2e8867a612ba271f513e76242
tarja2000 <mesterlevente00@gmail.com>
2026-05-06 16:59:43 +0200

    movie service javitas

--- Jegymester/JegymesterApp/app/blueprints/movie/service.py
+++ Jegymester/JegymesterApp/app/blueprints/movie/service.py
@@ -7,14 +7,19 @@ from app.models.movie import Movie
 class MovieService:
     @staticmethod
     def get_all():
-        movies = db.session.execute(select(Movie).order_by(Movie.id)).scalars().all()
+        movies = db.session.execute(
+            select(Movie).order_by(Movie.id)
+        ).scalars().all()
+
         return True, movies
 
     @staticmethod
     def get_by_id(movie_id: int):
         movie = db.session.get(Movie, movie_id)
+
         if movie is None:
             return False, "Nincs ilyen film"
+
         return True, movie
 
     @staticmethod
@@ -24,46 +29,54 @@ class MovieService:
                 name=data["name"],
                 description=data.get("description")
             )
+
             db.session.add(movie)
             db.session.commit()
+
             return True, movie
+
         except Exception as ex:
             db.session.rollback()
-            return False, f"Hiba a film létrehozásakor: {ex}"
+            return False, f"Hiba a film letrehozasakor: {ex}"
 
     @staticmethod
     def update(movie_id: int, data):
         try:
             movie = db.session.get(Movie, movie_id)
+
             if movie is None:
                 return False, "Nincs ilyen film"
 
             if "name" in data:
                 movie.name = data["name"]
+
             if "description" in data:
                 movie.description = data["description"]
 
             db.session.commit()
+
             return True, movie
+
         except Exception as ex:
             db.session.rollback()
-            return False, f"Hiba a film módosításakor: {ex}"
+            return False, f"Hiba a film modositasakor: {ex}"
 
     @staticmethod
     def delete(movie_id: int):
         try:
             movie = db.session.get(Movie, movie_id)
+
             if movie is None:
                 return False, "Nincs ilyen film"
 
             if movie.screenings:
-                return False, "A film nem törölhető, mert tartozik hozzá vetítés"
+                return False, "A film nem torolheto, mert tartozik hozza vetites"
 
             db.session.delete(movie)
             db.session.commit()
-            return True, {"message": "Film törölve"}
+
+            return True, {"message": "Film torolve"}
+
         except Exception as ex:
             db.session.rollback()
-            return False, f"Hiba a film törlésekor: {ex}"
-
-
+            return False, f"Hiba a film torlesekor: {ex}"
